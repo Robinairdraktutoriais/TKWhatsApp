f0c78ad7f7d8cb65f885689b1eb0cdb110d739b6
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2022-05-17 08:26:20 -0300

    Add files via upload

---
+++ clogs.js
@@ -0,0 +1,12 @@
+{
+    "pacotes": [
+        {   
+            "versão": "14.0",
+            "pacote": "com.tkwhatsapp",
+            "status": "update_title",
+            "title": "TKWhatsApp 14.0",
+            "nota": "falso",
+            "msgtxt": "https://raw.githubusercontent.com/Robinairdraktutoriais/TKWhatsApp/main/log.txt",
+            "baixar": "https://robin-airdrak-tutoriais9.webnode.com",
+            "atualizar": [
+                {
